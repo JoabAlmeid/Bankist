e7d9cfb72b8c86d01be622dcc8c8bb1fa069e434
Joab Almeida <joabalmeida.dev@gmail.com>
2025-03-18 23:02:34 -0300

    chainning methods

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const displayMovements = function (movements) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov}€</div>
       </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -83,9 +83,32 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 //pegar siglas do nome. Deixa tudo em minúsculo, divide em strings tudo entre os espaços, retorna a primeira letra, e depois junta tudo na mesma string. Feito um "side effect"(fazer algo e não retornar valor): todas as atuais accounts terão uma nova propriedade que são as siglas
 const createUsernames = function (accs) {
@@ -364,6 +387,7 @@ const calcAverageHumanAge3 = dogAge
   }, 0);
 
   */
+/*
 const calcAverageHumanAge = function (ages) {
   const calcAverageHumanAge4 = ages
     .map(function (age) {
@@ -387,3 +411,16 @@ const calcAverageHumanAge = function (ages) {
 // console.log(calcAverageHumanAge3);
 console.log(calcAverageHumanAge(dogAge1));
 console.log(calcAverageHumanAge(dogAge2));
+*/
+
+const euroToUsd = 1.1;
+console.log(movements);
+
+//PIPELINE (chained methods)
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    return mov * euroToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
